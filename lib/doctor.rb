767eed4df364562d8047754cfbcdd9e1ad88030b
Essa Hijazi <github email address>
2018-06-08 14:32:28 -0400

    Has Many Objects Through Lab

---
+++ lib/doctor.rb
@@ -0,0 +1,36 @@
+class Doctor 
+
+    attr_accessor :name
+    @@all = []
+
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+
+    def new_appointment(patient, doctor)
+        Appointment.new(self, patient, doctor)
+    end
+
+
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.doctor == self
+        end
+    end
+
+
+    def patients
+        self.appointments.collect do |appointment|
+            appointment.patient
+        end
+    end
+
+end
---
+++ lib/patient.rb
@@ -0,0 +1,27 @@
+class Patient
+
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def new_appointment(doctor, date)
+        Appointment.new(doctor, self, date)
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.patient == self
+        end
+    end
+
+    def doctors
+        self.appointments.collect do |appointment|
+            appointment.doctor
+        end
+    end
+end
